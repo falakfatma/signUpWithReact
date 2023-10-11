ee0996779ba008df68c2ab283e9c7a9d3bbf1580
falakfatma <102820083+falakfatma@users.noreply.github.com>
2023-10-11 18:11:40 +0000

    add function and fetch Json data.

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -10,12 +10,26 @@ export default function Signup() {
     e.preventDefault();
     console.log(email, name, password);
   }
+ const handleNameChange = (e) => {
+    
+  }
+ const handleEmailChange = (e) => {
+    
+  }
+ const handlePasswordChange = (e) => {
+    
+  }
+ const handleConfirmPasswordChange = (e) => {
+    
+  }
+  fetch("https://jsonplaceholder.typicode.com/users")
   return (
     <form type="active" className='sign_Form'>
-      <input placeholder="Name"  className="signup_Form_Items" value={name}/>
-      <input placeholder="Email" className="signup_Form_Items"  value={email}/>
-      <input placeholder="Password" className="signup_Form_Items"  value={password}/>
-      <button className="btn" type="submit" onClick={signUpHandler}>Submit</button>
+      <input placeholder="Name"  className="signup_Form_Items" onInput={handleNameChange} value={name}/>
+      <input placeholder="Email" className="signup_Form_Items" onInput={handleEmailChange} value={email}/>
+      <input placeholder="Create Password" className="signup_Form_Items" onInput={handlePasswordChange} value={password}/>
+      <input placeholder="Confirm Password" className="signup_Form_Items" onInput={handleConfirmPasswordChange} value={password}/>
+      <button className="btn submitBtn" type="submit" onClick={signUpHandler}>Submit</button>
     </form>
   )
 }
